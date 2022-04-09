c6ff0f93e14d0bd35eaab18d3bd38f720b11bbc2
Lucas Kunz <lucaskunz1@gmail.com>
2022-04-09 18:16:46 -0300

    Four challenge

--- src/__tests__/pages/Home.spec.tsx
+++ src/__tests__/pages/Home.spec.tsx
@@ -120,6 +120,8 @@ describe('Home page', () => {
       ],
     });
 
+    console.log(wrapper);
+
     render(<Home />, { wrapper });
 
     expect(await screen.findByText('The best doge')).toBeInTheDocument();
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,34 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [imageCurrentUrl, setImageCurrentUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string): void => {
+    onOpen();
+    setImageCurrentUrl(url);
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid templateColumns="repeat(3, 1fr)" gap="40">
+        {cards.map(card => {
+          return (
+            <Card
+              key={card.id}
+              data={card}
+              viewImage={() => handleViewImage(card.url)}
+            />
+          );
+        })}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={imageCurrentUrl}
+      />
     </>
   );
 }
